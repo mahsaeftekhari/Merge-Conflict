63a5cd28c47fa10d95c92ced91faefda53f87aee
TayebSafari <tssafri@gmail.com>
2021-06-04 12:00:24 +0430

    Add nameLabel and textBox and button in Form4

--- MyApplication/Form04.Designer.cs
+++ MyApplication/Form04.Designer.cs
@@ -29,19 +29,55 @@ namespace MyApplication
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.SuspendLayout();
-			// 
-			// Form04
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 361);
-			this.Location = new System.Drawing.Point(0, 0);
-			this.Name = "Form04";
-			this.ResumeLayout(false);
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.testTextBox = new System.Windows.Forms.TextBox();
+            this.testButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 9);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(60, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Mitra Safari";
+            // 
+            // testTextBox
+            // 
+            this.testTextBox.Location = new System.Drawing.Point(12, 25);
+            this.testTextBox.Name = "testTextBox";
+            this.testTextBox.Size = new System.Drawing.Size(100, 20);
+            this.testTextBox.TabIndex = 1;
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(12, 51);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(75, 23);
+            this.testButton.TabIndex = 2;
+            this.testButton.Text = "Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            // 
+            // Form04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.testTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Form04";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox testTextBox;
+        private System.Windows.Forms.Button testButton;
+    }
 }
